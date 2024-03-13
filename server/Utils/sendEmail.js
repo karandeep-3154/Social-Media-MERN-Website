d6b57b60c4312525fdbba5a66c82440eff0ea655
karandeep-3154 <ksingh310504@gmail.com>
2024-03-13 18:01:40 +0530

    Reset Password Using Email Verification Functionality Implemented

--- server/Utils/sendEmail.js
+++ server/Utils/sendEmail.js
@@ -83,9 +83,9 @@ token;
 
 export const resetPasswordLink = async (user, res) => {
   const { _id, email } = user;
-
+console.log(user.email, user.firstName, user.lastName);
   const token = _id + uuidv4();
-  const link = APP_URL + "users/reset-password/" + _id + "/" + token;
+  const link = APP_URL + "/users/reset-password/" + _id + "/" + token;
 
   //   mail options
   const mailOptions = {
