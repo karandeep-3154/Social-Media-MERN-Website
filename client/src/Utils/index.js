15012b3bd88cabe6882c9fe38ed289d3b1e1f58f
karandeep-3154 <ksingh310504@gmail.com>
2024-03-21 10:23:28 +0530

    Server URI updated after Deployment

--- client/src/Utils/index.js
+++ client/src/Utils/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { SetPosts } from '../redux/postSlice';
-const API_URL = 'http://localhost:8080/';
+const API_URL = 'https://social-media-mern-website.onrender.com/';
 
 export const API = axios.create({
     baseURL: API_URL,
